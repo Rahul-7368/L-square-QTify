4cb9f8d514b14b87e6fdaef771248569fdfe71c1
Rahul-7368 <kumarrahul.godda@gmail.com>
2024-05-28 16:10:04 +0530

    final qtify

---
+++ qtify-project/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../../Assets/logo.png";
+const Logo = () => {
+  return <img src={logo} width={67} alt="logo not available" />;
+};
+
+export default Logo;
+
+
+
