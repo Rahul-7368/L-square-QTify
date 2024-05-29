aebf54d115b4c4b0aa2e8a8a467866c528b34c21
Rahul-7368 <kumarrahul.godda@gmail.com>
2024-05-29 11:20:11 +0530

    final commit

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../Assets/logo.png";
+import "./logo.css";
 const Logo = () => {
   return <img src={logo} width={67} alt="logo not available" />;
 };
