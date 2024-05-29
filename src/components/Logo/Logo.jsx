73b5e63bf13ed71a99fa6aac73fa270343ecf3e2
Rahul-7368 <kumarrahul.godda@gmail.com>
2024-05-29 13:37:02 +0530

    final commit

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import logo from "../../Assets/logo.png";
 import "./logo.css";
 const Logo = () => {
-  return <img src={logo} width={67} alt="logo not available" />;
-};
+  return(
+    <div class="img-container">
+    <img src={logo} width={67} alt="logo not available" />
+  
+    </div>
+  )
+
+  // return <img src={logo} width={67} alt="logo not available" />;
+}
 
 export default Logo;
 
